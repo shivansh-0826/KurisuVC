b5fe290f9db6bb9a2f06c38d831e21325e82971d
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-07-30 21:19:11 +0530

    Update __init__.py

--- DaisyXMusic/__init__.py
+++ DaisyXMusic/__init__.py
@@ -8,7 +8,7 @@ from pyrogram import Client, idle
 from DaisyXMusic.config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client(
-    "ErenPyro",
+    "SatoruPyro",
    api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
