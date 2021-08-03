9bd8844d9b36412d8332d35d5c7ee3f8f1f30cd1
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-08-03 10:38:13 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -1173,7 +1173,7 @@ async def lol_cb(b, cb):
     if cb.from_user.id != useer_id:
         await cb.answer("You ain't the person who requested to play the song!", show_alert=True)
         return
-    await cb.message.edit("Mikasa is Getting ready to play music hang on.....")
+    await cb.message.edit("Kurisu Is Getting Ready To Play Music, Hang On.....")
     x=int(x)
     try:
         useer_name = cb.message.reply_to_message.from_user.first_name
